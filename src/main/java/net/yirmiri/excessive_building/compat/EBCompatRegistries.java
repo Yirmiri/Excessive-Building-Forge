1134db62c0abd0b95f83f73d8e871a9a0988f12e
yirmiri <yirmirilucy@gmail.com>
2024-02-18 00:37:14 -0500

    Compatibility fixes

--- src/main/java/net/yirmiri/excessive_building/compat/EBCompatRegistries.java
+++ src/main/java/net/yirmiri/excessive_building/compat/EBCompatRegistries.java
@@ -2,21 +2,17 @@ package net.yirmiri.excessive_building.compat;
 
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.ModList;
-import net.yirmiri.excessive_building.compat.hearthandhome.HearthAndHomeCompat;
 import net.yirmiri.excessive_building.compat.sullysmod.SullysModCompat;
 
 public class EBCompatRegistries {
     public static final boolean sullysMod;
-    public static final boolean hearthAndHome;
 
     static {
         ModList mods = ModList.get();
         sullysMod = mods.isLoaded("sullysmod");
-        hearthAndHome = mods.isLoaded("hearth_and_home");
     }
 
     public static void register(IEventBus eventBus) {
         if (sullysMod) SullysModCompat.register(eventBus);
-        if (hearthAndHome) HearthAndHomeCompat.register(eventBus);
     }
 }
