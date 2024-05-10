a89b47566f0182baba4c0a329c319460ef9db08a
yirmiri <yirmirilucy@gmail.com>
2024-05-09 20:16:52 -0400

    Ancient Leaves 2.0

--- src/main/java/net/yirmiri/excessive_building/block/AncientLeavesBlock.java
+++ src/main/java/net/yirmiri/excessive_building/block/AncientLeavesBlock.java
@@ -1,18 +1,80 @@
 package net.yirmiri.excessive_building.block;
 
+import net.minecraft.ChatFormatting;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
+import net.minecraft.network.chat.CommonComponents;
+import net.minecraft.network.chat.Component;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
 import net.minecraft.util.ParticleUtils;
 import net.minecraft.util.RandomSource;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.LeavesBlock;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.BooleanProperty;
+import net.minecraft.world.phys.BlockHitResult;
+import net.yirmiri.excessive_building.datagen.EBItemTagProvider;
 import net.yirmiri.excessive_building.register.EBParticleTypes;
+import org.jetbrains.annotations.NotNull;
+
+import javax.annotation.Nullable;
+import java.util.List;
 
 public class AncientLeavesBlock extends LeavesBlock {
+    public static final BooleanProperty GLOWING = BooleanProperty.create("glowing");
+
     public AncientLeavesBlock(BlockBehaviour.Properties properties) {
         super(properties);
+        registerDefaultState(defaultBlockState().setValue(GLOWING, false));
+    }
+
+    @Override
+    public void appendHoverText(ItemStack stack, @Nullable BlockGetter getter, List<Component> list, TooltipFlag flag) {
+        super.appendHoverText(stack, getter, list, flag);
+        list.add(CommonComponents.EMPTY);
+        list.add(Component.translatable("tooltip.block.interact_glow_ink").withStyle(ChatFormatting.GRAY));
+        list.add(CommonComponents.space().append(Component.translatable("tooltip.block.glow").withStyle(ChatFormatting.BLUE)));
+    }
+
+    @Override @NotNull
+    public InteractionResult use(BlockState state, Level level, BlockPos pos, Player player, InteractionHand hand, BlockHitResult hit) {
+        ItemStack itemstack = player.getItemInHand(hand);
+        if (!state.getValue(GLOWING) && itemstack.is(EBItemTagProvider.GLOW_PASTES)) {
+            addGlow(state, level, pos);
+            if (!player.isCreative()) {
+                itemstack.shrink(1);
+            }
+        } else {
+            return InteractionResult.CONSUME;
+        }
+        return InteractionResult.sidedSuccess(level.isClientSide);
+    }
+
+    private static void addGlow(BlockState state, Level level, BlockPos pos) {
+        if (!level.isClientSide) {
+            level.setBlockAndUpdate(pos, state.setValue(GLOWING, true));
+            level.playSound(null, pos, SoundEvents.GLOW_INK_SAC_USE, SoundSource.BLOCKS, 1, 1);
+        }
+    }
+
+    public static boolean isGlowing(BlockState state) {
+        return state.getValue(GLOWING);
+    }
+
+    @Override
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
+        super.createBlockStateDefinition(builder);
+        builder.add(GLOWING);
     }
 
     public void animateTick(BlockState blockState, Level level, BlockPos blockPos, RandomSource randomSource) {
