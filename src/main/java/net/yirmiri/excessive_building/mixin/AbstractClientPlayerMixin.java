62d127fc53e6e730f5d9a9eee2f8232143d91e85
yirmiri <yirmirilucy@gmail.com>
2024-05-11 16:02:27 -0400

    SWAG DRIP PURPLE CURSE

--- src/main/java/net/yirmiri/excessive_building/mixin/AbstractClientPlayerMixin.java
+++ src/main/java/net/yirmiri/excessive_building/mixin/AbstractClientPlayerMixin.java
@@ -20,6 +20,7 @@ public abstract class AbstractClientPlayerMixin extends Player {
     private static final List<String> AZURUNE = List.of("1cedf927-5c8f-4650-95e9-808fc8f94d00", "bd35c402-fa9c-4d00-afe6-b4ed9ebe90c4"); //Developers
     private static final List<String> BEETROOT = List.of("9778ff53-d83d-4233-8fa6-8aab7b89c4c0"); //Special thank you for helping me with some assets at a very low point
     private static final List<String> DEMONHEART = List.of("c12df14d-24ed-4247-84e8-e10c111237df"); //Special thank you for helping me with some assets at a very low point
+    private static final List<String> ACCURSED = List.of("416fc916-69cc-4b3c-8c5e-a39a5acb6981"); //Swagified knitted purple wool (& knitted green wool)
     private static final List<String> DICE = List.of("3fd1d511-62d6-4e18-a28d-3e3d4fd93620"); //Helped out with dyed blocks & datagen shenanigans
     private static final List<String> HEX = List.of("452ec9e4-a4f8-4edf-bd3c-ab3d7b751359"); //j++
     private static final List<String> FUDGE_SUNDAE = List.of("7a6a8c68-8b73-47f6-b08f-0dde5f1848dd"); //This guy should not have a cape
@@ -37,6 +38,7 @@ public abstract class AbstractClientPlayerMixin extends Player {
         if (AZURUNE.contains(stringUUID)) cir.setReturnValue(new ResourceLocation(ExcessiveBuilding.MODID, "textures/capes/azurune.png"));
         if (BEETROOT.contains(stringUUID)) cir.setReturnValue(new ResourceLocation(ExcessiveBuilding.MODID, "textures/capes/beetroot.png"));
         if (DEMONHEART.contains(stringUUID)) cir.setReturnValue(new ResourceLocation(ExcessiveBuilding.MODID, "textures/capes/demonheart.png"));
+        if (ACCURSED.contains(stringUUID)) cir.setReturnValue(new ResourceLocation(ExcessiveBuilding.MODID, "textures/capes/accursed.png"));
         if (DICE.contains(stringUUID)) cir.setReturnValue(new ResourceLocation(ExcessiveBuilding.MODID, "textures/capes/dice.png"));
         if (HEX.contains(stringUUID)) cir.setReturnValue(new ResourceLocation(ExcessiveBuilding.MODID, "textures/capes/hex.png"));
         if (FUDGE_SUNDAE.contains(stringUUID)) cir.setReturnValue(new ResourceLocation(ExcessiveBuilding.MODID, "textures/capes/fudge_sundae.png"));
